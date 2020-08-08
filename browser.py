3c5bb137ba0882c638a29d8b08325eaef6fd84e2
Mikhayil <58036447+Miskatop@users.noreply.github.com>
2020-08-08 12:57:32 +0400

    update browser and add the featuers

--- browser.py
+++ browser.py
@@ -19,12 +19,18 @@ tabWidget = QTabWidget()
 # elements
 url_line = QLineEdit()
 add_tab_button =  QPushButton("+")
+CloseTab = QPushButton("X")
 search_btn = QPushButton("Search")
 back_btn = QPushButton("Back")
+progressBar = QProgressBar()
 
-# variables
-current_url = 'https://www.google.com'
 
+# settings
+progressBar.setMaximum(100)
+progressBar.setTextVisible(False)
+progressBar.setValue(0)
+
+# functions
 def to_google(args):
 	url = 'https://google.com/search?q='
 	for arg in args :
@@ -47,25 +53,46 @@ def add_tab():
 def construct_tabs():
 	for i in web_eng:
 		web_eng[web_eng.index(i)].load(QUrl(urls[web_eng.index(i)]))
-		tabWidget.addTab(web_eng[web_eng.index(i)], current_url)
+		web_eng[web_eng.index(i)].loadStarted.connect(title)
+		web_eng[web_eng.index(i)].loadProgress.connect(loading)
+		web_eng[web_eng.index(i)].loadFinished.connect(lambda x: progressBar.setValue(0))
+		tabWidget.addTab(web_eng[web_eng.index(i)], title())
 		tabWidget.setCurrentIndex(web_eng.index(i))
 
+def close_tab():
+	x = tabWidget.currentIndex()
+	del web_eng[x]
+	del urls[x]
+	tabWidget.removeTab(x)
 
-# Functions
-def title(_title):
-	win.setWindowTitle('WebLab - ' + str(_title.split('//')[-1]))	
-	url_line.setText(_title)
+def loading(proc):
+	progressBar.setValue(proc)
 
+def title():
+	urls[tabWidget.currentIndex()] = web_eng[tabWidget.currentIndex()].url().toString()
+	
+	show_url = urls[tabWidget.currentIndex()]
+	if len(show_url) > 30:
+		show_url = str(urls[tabWidget.currentIndex()])[:30]
+
+	win.setWindowTitle('WebLab - ' + show_url)	
+	url_line.setText(urls[tabWidget.currentIndex()])
+	tabWidget.setTabText(tabWidget.currentIndex() ,show_url)
+
+# handlers
 add_tab_button.clicked.connect(add_tab)
+CloseTab.clicked.connect(close_tab)
 search_btn.clicked.connect(search)
 
 def window():
-	web_eng[0].load(QUrl(current_url))
+	web_eng[0].load(QUrl(urls[tabWidget.currentIndex()]))
 	win.setWindowState(Qt.WindowMaximized)
 	hbox.addWidget(url_line)
 	hbox.addWidget(search_btn)
 	hbox.addWidget(back_btn)
 	hbox.addWidget(add_tab_button)
+	hbox.addWidget(CloseTab)
+	hbox.addWidget(progressBar)
 	vbox.addLayout(hbox)
 	construct_tabs()
 	vbox.addWidget(tabWidget)
